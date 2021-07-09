c3c1ab3cd4f3c93a7f9b1ce8f9b3358d856d7492
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-09 15:49:30 +0530

    PasswordGenerator function

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,51 @@
 module.exports = () => {
 
     function passwordGenerator(){
-
+        const uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const lowercaseAlphabet = "abcdefghijklmnopqrstuvwxyz";
+        const number = "0123456789";
+        const specialCharacter = "~!@#$%^&*()-+?</>"
+        if(uppercaseAlphabetIsActive){
+            let text = uppercaseAlphabet+number;        
+            let result = '';
+            for (let i = 0; i < n; i++) result += text[Math.round(Math.random() * (text.length - 1))];
+            return result;
+        } else if(lowercaseAlphabetIsActive){
+            let text = lowercaseAlphabet+number;        
+            let result = '';
+            for (let i = 0; i < n; i++) result += text[Math.round(Math.random() * (text.length - 1))];
+            return result;
+        } else if(specialCharacterIsActive){
+            let text = specialCharacter+number;        
+            let result = '';
+            for (let i = 0; i < n; i++) result += text[Math.round(Math.random() * (text.length - 1))];
+            return result;
+        } else if(uppercaseAlphabetIsActive && lowercaseAlphabetIsActive){
+            let text = uppercaseAlphabet+lowercaseAlphabet+number;        
+            let result = '';
+            for (let i = 0; i < n; i++) result += text[Math.round(Math.random() * (text.length - 1))];
+            return result;
+        } else if(uppercaseAlphabetIsActive && specialCharacterIsActive){
+            let text = uppercaseAlphabet+specialCharacter+number;        
+            let result = '';
+            for (let i = 0; i < n; i++) result += text[Math.round(Math.random() * (text.length - 1))];
+            return result;
+        } else if(specialCharacterIsActive && lowercaseAlphabetIsActive){
+            let text = specialCharacter+lowercaseAlphabet+number;        
+            let result = '';
+            for (let i = 0; i < n; i++) result += text[Math.round(Math.random() * (text.length - 1))];
+            return result;
+        } else if(uppercaseAlphabetIsActive && lowercaseAlphabetIsActive && specialCharacterIsActive){
+            let text = uppercaseAlphabet+lowercaseAlphabet+specialCharacter+number;        
+            let result = '';
+            for (let i = 0; i < n; i++) result += text[Math.round(Math.random() * (text.length - 1))];
+            return result;
+        } else {
+            let text = number;        
+            let result = '';
+            for (let i = 0; i < n; i++) result += text[Math.round(Math.random() * (text.length - 1))];
+            return result;    
+        }
     }
 
 
