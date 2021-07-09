7b77ec96f83377d5ae7f3d0dbf59a23e1f35416d
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-09 13:51:07 +0530

    index setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express()
+const port = 5000;
+var apiRouter = require("./routes/api");
+
+app.use(apiRouter)
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+var mongoUrl = 'mongodb://127.0.0.1/ecommerceDB'
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+});
+
+
+app.listen(port, ()=>{
+    console.log(`app listening at http://localhost:${port}`)
+})
