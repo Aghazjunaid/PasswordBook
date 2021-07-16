5ed68cca79d838845aea9b9c89997e71ff2c4041
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-16 22:33:04 +0530

    jwt

--- utils/utils.js
+++ utils/utils.js
@@ -1,4 +1,21 @@
 module.exports = () => {
+    const jwt = require('jsonwebtoken');
+
+    function authenticateToken(req, res, next) {
+        const token = req.headers['authorization'];
+        if (!token){
+            return res.status(401).send("Invalid authorization");
+        } else {
+            jwt.verify(token, "gsjkah35gsj546b5t", (error, user) => {
+                if (error){
+                    return res.status(400).send(error);
+                }
+                req.user = user;
+                next();
+            })
+        }
+    }
+
 
     function passwordGenerator(passwordLength,uppercaseAlphabetIsActive,lowercaseAlphabetIsActive,specialCharacterIsActive){
         const uppercaseAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -48,11 +65,8 @@ module.exports = () => {
         }
     }
 
-
-
-
-
     return {
-        passwordGenerator
+        passwordGenerator,
+        authenticateToken
     }
 }
