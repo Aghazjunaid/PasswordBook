5bc0608cc5c9ef836bb429adccde341118b8d892
Aghaz1-alphonic <aghaz.junaid@alphonic.in>
2021-07-25 12:39:58 +0530

    get pass by id

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,7 @@
 var express = require('express'),
 apiRouter = express.Router();
 
-utils = require('../utils')();
+utils = require('../utils/utils')();
 user = require('./user')();
 passwordGenerator = require('./generatePassword')();
 passwordBook = require('./passwordBook')();
@@ -18,6 +18,7 @@ apiRouter.get('/login', user.loginUser);
 //===========Password Book API==============
 apiRouter.post('/password', utils.authenticateToken, passwordBook.addPassword);
 apiRouter.get('/password', utils.authenticateToken, passwordBook.getPassword);
+apiRouter.get('/password/:id', utils.authenticateToken, passwordBook.getPasswordById);
 apiRouter.put('/password/:id', utils.authenticateToken, passwordBook.updatePassword);
 apiRouter.delete('/password/:id', utils.authenticateToken, passwordBook.deletePassword);
 
